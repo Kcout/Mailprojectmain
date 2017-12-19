0b45e3feeb2edf248105fbc2ca1ac51ab6624a45
Kcout <cout.kevin.pro@gmail.com>
2017-12-19 17:48:31 +0100

    first commit git init git add README.md git commit -m "first commit" git remote add origin https://github.com/Kcout/Mailprojectmain.git git push -u origin master

---
+++ 2PLAY/Indexconnexion.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>2 PLAY TENNIS</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+
+<body id="top">
+
+    <!-- Header -->
+    <header id="header">
+        <h1>2 PLAY TENNIS</h1>
+        <div class="inner">
+            <a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+        </div>
+    </header>
+
+    <!-- Main -->
+    <div id="main">
+
+    <nav>
+        <ul>
+            <li><img src="images/Logo.png" alt="logo"</a></li>
+                <li id="home">Accueil</li>
+                <li id="Actu">Actualités</li>
+                <li id="classement">Classement</li>
+                <li id="Part">Evenements</li>
+                <li id="contact">Contacte</li>
+        </ul>
+    </nav>                  
+            <!-- Footer -->
+            <footer id="footer">
+                <div class="inner">
+                    <ul class="icons">
+                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+                        <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+                        <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+                    </ul>
+                </div>
+            </footer>
+
+            <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/jquery.poptrox.min.js"></script>
+            <script src="assets/js/skel.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+            <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
